d3cd4c92057ed6328a467fc5875de0e257dba323
Aneirine <anyashi2002@gmail.com>
2020-08-05 14:20:17 +0300

    Add new route

--- src/main/java/com/aneirine/reactor/config/GreetingRouter.java
+++ src/main/java/com/aneirine/reactor/config/GreetingRouter.java
@@ -4,6 +4,7 @@ import com.aneirine.reactor.handler.GreetingHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
@@ -15,7 +16,14 @@ public class GreetingRouter {
     @Bean
     public RouterFunction<ServerResponse> routerFunction(GreetingHandler handler) {
         return RouterFunctions.route(RequestPredicates.GET("/hello")
-                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), handler::hello);
+                .and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), handler::hello)
+                .andRoute(RequestPredicates.GET("/"),
+                        serverRequest -> ServerResponse
+                                .ok()
+                                 .contentType(MediaType.APPLICATION_JSON)
+                                .body(
+                                        BodyInserters.fromValue("Hi!")
+                                ));
 
     }
 }
