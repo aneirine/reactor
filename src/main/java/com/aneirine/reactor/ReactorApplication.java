9d007a129e0ca9b03ffc85d1e1ab61b47c640751
Aneirine <anyashi2002@gmail.com>
2020-08-05 13:51:42 +0300

    rename package

--- src/main/java/com/aneirine/reactor/ReactorApplication.java
+++ src/main/java/com/aneirine/reactor/ReactorApplication.java
@@ -1,4 +1,4 @@
-package com.aneirinne.reactor;
+package com.aneirine.reactor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/aneirine/reactor/ReactorApplicationTests.java
+++ src/test/java/com/aneirine/reactor/ReactorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.aneirinne.reactor;
+package com.aneirine.reactor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
