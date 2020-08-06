4e9dc32f49c8d2e447a37c24be79d4b47704f249
Aneirine <anyashi2002@gmail.com>
2020-08-06 19:48:22 +0300

    Add endpoints

--- src/main/java/com/aneirine/reactor/controllers/MessageController.java
+++ src/main/java/com/aneirine/reactor/controllers/MessageController.java
@@ -1,4 +1,31 @@
 package com.aneirine.reactor.controllers;
 
+import com.aneirine.reactor.models.Message;
+import com.aneirine.reactor.services.MessageService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/controller/messages")
 public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+
+    @GetMapping
+    public Flux<Message> getList(@RequestParam("start") int start, @RequestParam("count") int count){
+        return messageService.findAll();
+    }
+
+    @PostMapping
+    public Mono<Message> createMessage(Message message){
+        return messageService.createMessage(message);
+    }
+
+
 }
