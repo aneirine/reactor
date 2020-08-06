6cd560b70ecdc4cf5940d83c25d0873b4387de5e
Aneirine <anyashi2002@gmail.com>
2020-08-06 19:39:08 +0300

    Add methods to message servie

--- src/main/java/com/aneirine/reactor/services/MessageService.java
+++ src/main/java/com/aneirine/reactor/services/MessageService.java
@@ -1,4 +1,27 @@
 package com.aneirine.reactor.services;
 
+import com.aneirine.reactor.models.Message;
+import com.aneirine.reactor.repo.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public Flux<Message> findAll(){
+        return messageRepository.findAll();
+    }
+
+    public Mono<Message> createMessage(Message message){
+        return messageRepository.save(message);
+    }
 }
