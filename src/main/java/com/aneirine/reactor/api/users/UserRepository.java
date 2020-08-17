74215a15e5d095ebcca28723e862f6818a10444e
Aneirine <anyashi2002@gmail.com>
2020-08-17 12:37:53 +0300

    Add service

--- src/main/java/com/aneirine/reactor/api/users/UserRepository.java
+++ src/main/java/com/aneirine/reactor/api/users/UserRepository.java
@@ -2,6 +2,9 @@ package com.aneirine.reactor.api.users;
 
 import com.aneirine.reactor.entities.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+
+    Mono<User> findByUsername(String username);
 }
